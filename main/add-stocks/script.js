7793e832f8fa067bf112975adde280b854bce58a
Marc <116419409+SnakeyCoder25@users.noreply.github.com>
2023-06-04 10:24:44 +1000

    Stocks now stored in local

--- main/add-stocks/script.js
+++ main/add-stocks/script.js
@@ -5,8 +5,8 @@ localStorage.setItem("1", x1);
 let x2 = document.getElementById("msg2").value;
 localStorage.setItem("2", x2);
 let x3 = document.getElementById("msg3").value;
-localStorage.setItem("3", x2);
+localStorage.setItem("3", x3);
 let x4 = document.getElementById("msg4").value;
-localStorage.setItem("4", x2);
+localStorage.setItem("4", x4);
 }
 //localStorage.getElementById = document.getElementById("One");
