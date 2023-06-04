6489ec63f2b9ad29c56338a2c969de0db3006701
Marc <116419409+SnakeyCoder25@users.noreply.github.com>
2023-06-04 10:20:34 +1000

    centred table

--- main/add-stocks/script.js
+++ main/add-stocks/script.js
@@ -1,6 +1,12 @@
-let x1 = document.getElementById("msg").value;
 
-
+function updateLocaldata() {
+let x1 = document.getElementById("msg1").value;
 localStorage.setItem("1", x1);
-
-localStorage.getElementById = document.getElementById("one")
+let x2 = document.getElementById("msg2").value;
+localStorage.setItem("2", x2);
+let x3 = document.getElementById("msg3").value;
+localStorage.setItem("3", x2);
+let x4 = document.getElementById("msg4").value;
+localStorage.setItem("4", x2);
+}
+//localStorage.getElementById = document.getElementById("One");
